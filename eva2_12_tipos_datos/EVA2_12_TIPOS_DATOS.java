b101df88325aa63bb31a041ab200582f732d78c7
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-11-02 10:58:06 -0600

    Add files via upload

---
+++ eva2_12_tipos_datos/EVA2_12_TIPOS_DATOS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_tipos_datos;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA2_12_TIPOS_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte valor = -128;
+        byte valor2 = 127;
+        
+        System.out.println("Valor = "+ (valor));
+         System.out.println("Valor = "+ (valor2));
+         short valor = -32768;
+         short valor = 32767;
+         int x = -2147483618;
+          int y = 2147483617;
+          
+    }
+    
+}
